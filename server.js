29543b74fdce4f1c7416dbffb92c1f8027f13af1
qez <me@me.com>
2018-09-18 23:14:55 -0500

    hello world now works

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const app = express();
-const port = 8080;
-const io = socketIO(express);
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
 
-app.use((req,res) => res.sendFile('index.html')).listen(PORT, () => console.log('listening on 8080'));
+app.use(express.static(__dirname + '/node_modules'));
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
 
-io.on('connection', (socket) => {
-	console.log('client connected');
-	socket.on('disconnect', () => console.log('client disconnected'));
+//server.listen(8080);
+
+io.on('connection', function(socket){
+	console.log("client connected");
+});
+
+server.listen(8080, function(){
+	console.log('listening on port 8080');
 });
