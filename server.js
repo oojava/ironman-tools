3fae7938f64e788300120bff237d042faf58c0b0
qez <me@me.com>
2018-09-18 22:57:11 -0500

    take two, with socket.io this time

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 8080;
+const io = socketIO(express);
 
-app.get('/', (req, res) => res.send('HelloWorld'));
+app.use((req,res) => res.sendFile('index.html')).listen(PORT, () => console.log('listening on 8080'));
 
-app.listen(port, () => console.log('example app listing on port ${port}'));
+io.on('connection', (socket) => {
+	console.log('client connected');
+	socket.on('disconnect', () => console.log('client disconnected'));
+});
