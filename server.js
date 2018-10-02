063718cfcbd2cd208bd21f642b92e6f09b003df0
qez <me@me.com>
2018-10-02 02:42:13 -0500

    Added in the icon

--- server.js
+++ server.js
@@ -2,8 +2,15 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var path = require('path');
+
+var osrsHs = require("osrs-wrapper");
+//const {constants, hiscores} = require('osrs-api');
 
 app.use(express.static(__dirname + '/node_modules'));
+//app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static('public'));
+
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
 });
@@ -12,6 +19,16 @@ app.get('/', function(req, res){
 
 io.on('connection', function(socket){
 	console.log("client connected");
+	socket.on('nameLookup', function (msg) {
+			io.emit('statsRecieved', "sent");
+			console.log("the name is: " + msg);
+			osrsHs.hiscores.getPlayer(msg).then(player=> {
+				console.log(player);
+//				console.log(player["Skills"]);
+				io.emit('statsRecieved', player["Skills"]['Attack'].level);
+				io.emit('statsRecieved',"test");
+			});
+	});
 });
 
 server.listen(process.env.PORT || 8080, function(){
